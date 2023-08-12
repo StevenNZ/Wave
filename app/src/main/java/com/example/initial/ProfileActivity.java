de517d84bfff146c2e2f95c3ede9a6c30a24aead
sli521 <sli521@aucklanduni.ac.nz>
2023-08-12 21:08:42 +1200

    feat: sign up implementation

--- app/src/main/java/com/example/initial/ProfileActivity.java
+++ app/src/main/java/com/example/initial/ProfileActivity.java
@@ -2,8 +2,13 @@ package com.example.initial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,11 +18,22 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         TextView usernameGreet = findViewById(R.id.profileUsername);
+        Button signoutBtn = findViewById(R.id.signoutBtn);
 
         String receivedString = getIntent().getStringExtra("USERNAME");
         if (receivedString != null) {
             String greet = "Hi " + receivedString + "!";
             usernameGreet.setText(greet);
         }
+
+        signoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
