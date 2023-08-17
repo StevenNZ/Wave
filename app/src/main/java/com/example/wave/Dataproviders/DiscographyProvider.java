f76346bcf62e228240bce8c75f319a59dfef278c
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-18 01:59:22 +1200

    feat: Implemented discography repository method
    Implemented discography repository method

--- app/src/main/java/com/example/wave/Dataproviders/DiscographyProvider.java
+++ app/src/main/java/com/example/wave/Dataproviders/DiscographyProvider.java
@@ -3,18 +3,21 @@ package com.example.wave.Dataproviders;
 import com.example.wave.Entities.Discography;
 import com.example.wave.Entities.DiscographyForm;
 import com.example.wave.Repository.DiscographyRepository;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
 
 public interface DiscographyProvider {
-    DiscographyRepository getInstance();
-    List<Discography> getAllDiscography(String discographyID);
-    List<Discography> getDiscographyByDiscographyID(String discographyID);
-    List<Discography> getDiscographyBySearch(String searchString);
-    List<Discography> getDiscographyByCategoryID(String categoryID);
-    List<Discography> getDiscographyByArtistID(String artistID);
-    List<Discography> getDiscographyByCategory(String categoryID);
-    List<DiscographyForm> getDiscographyHardMediaForm(String discographyID);
+    static DiscographyRepository getInstance() {
+        return null;
+    }
+
+    Task<List<Discography>> getAllDiscography(String discographyID);
+    Task<Discography> getDiscographyByDiscographyID(String discographyID);
+    Task<List<Discography>> getDiscographyBySearch(String searchString);
+    Task<Discography> getDiscographyByCategoryID(String categoryID);
+    Task<Discography> getDiscographyByArtistID(String artistID);
+    Task<List<DiscographyForm>> getDiscographyHardMediaForm(String discographyID);
 }
 
