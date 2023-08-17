d8c64035485f0993229ea8f5a047d975fc8604e1
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-17 17:43:45 +1200

    feat: Implemented the cart repository
    Implemented the cart repository (except checkout of the cart)

--- app/src/main/java/com/example/wave/Dataproviders/CartProvider.java
+++ app/src/main/java/com/example/wave/Dataproviders/CartProvider.java
@@ -2,17 +2,22 @@ package com.example.wave.Dataproviders;
 
 import com.example.wave.Entities.Order;
 import com.example.wave.Repository.CartRepository;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
 
 
 public interface CartProvider {
-    CartRepository getInstance();
-    List<Order> getCart();
-    List<Order> removeCartItem(String discographyFormID);
-    List<Order> addItemToCart(Order cartOrder);
-    Order checkoutCart(String orderID);
+    static CartRepository getInstance() {
+        return null;
+    }
+
+    void createCart(String userID);
+    Task<Order> getCart(String userID);
+    void addCartItems(String userID, String discographyFormID);
+    void removeCartItems(String userID, String discographyFormID);
+    void checkoutCart(String userID);
 }
 
 
