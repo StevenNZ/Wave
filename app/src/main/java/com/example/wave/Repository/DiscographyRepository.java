0868d6b6e0b36c0ade260a575ff1533cb6889ce8
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-22 21:55:51 +1200

    Fix: Fixed case sensitivity of fuzzy search
    Fixed case sensitivity of fuzzy search

--- app/src/main/java/com/example/wave/Repository/DiscographyRepository.java
+++ app/src/main/java/com/example/wave/Repository/DiscographyRepository.java
@@ -109,7 +109,7 @@ public class DiscographyRepository implements DiscographyProvider {
                     Discography discography = documentSnapshot.toObject(Discography.class);
 
                     // Perform fuzzy search logic (e.g., check if productName contains searchTerm)
-                    if (discography != null && discography.getReleaseName().contains(searchString)) {
+                    if (discography != null && discography.getReleaseName().toLowerCase().contains(searchString.toLowerCase())) {
                         matchingDiscography.add(discography);
                     }
                 }
