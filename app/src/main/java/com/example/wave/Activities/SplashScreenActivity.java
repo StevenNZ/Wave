cc2fc7fdb85ee552ab835c1126b3130e407ac8af
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-23 02:20:41 +1200

    Feat: Implemented Initial Loading Screen
    Implemented Initial Loading Screen

---
+++ app/src/main/java/com/example/wave/Activities/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.wave.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wave.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
