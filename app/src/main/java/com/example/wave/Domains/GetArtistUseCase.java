7784ea53038fa50f3b76503ca0268821b3efd523
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-23 02:28:07 +1200

    Feat: Added Artist Usecase
    Added Artist Usecase

---
+++ app/src/main/java/com/example/wave/Domains/GetArtistUseCase.java
@@ -0,0 +1,20 @@
+package com.example.wave.Domains;
+
+import com.example.wave.Dataproviders.ArtistProvider;
+import com.example.wave.Entities.Artist;
+import com.example.wave.Repository.ArtistRepository;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+
+public class GetArtistUseCase implements ArtistProvider {
+    @Override
+    public Task<List<Artist>> getAllArtists() {
+        return ArtistRepository.getInstance().getAllArtists();
+    }
+
+    @Override
+    public Task<Artist> getArtistByID(String categoryID) {
+        return null;
+    }
+}
