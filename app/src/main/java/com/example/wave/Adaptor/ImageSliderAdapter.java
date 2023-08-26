fc76ba3ef2b2c326b2841a43c51e70123e777c30
sli521 <sli521@aucklanduni.ac.nz>
2023-08-26 15:42:42 +1200

    refactor: image slider now uses adaptor and glide

---
+++ app/src/main/java/com/example/wave/Adaptor/ImageSliderAdapter.java
@@ -0,0 +1,56 @@
+package com.example.wave.Adaptor;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.wave.R;
+
+import java.util.List;
+
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.ImageViewHolder> {
+
+    private List<String> imageUrls;
+    private Context context;
+    private ViewPager2 viewPager;
+
+    public ImageSliderAdapter(Context context, List<String> imageUrls, ViewPager2 viewPager) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+        this.viewPager = viewPager;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewpager_item, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        String imageUrl = imageUrls.get(position);
+        Glide.with(context)
+                .load(imageUrl)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.size();
+    }
+
+    class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        ImageViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.vpImage);
+        }
+    }
+}
+
