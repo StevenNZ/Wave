f8d3d029e74835765a87751bd60e31922d83fe21
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-08-27 15:55:20 +1200

    Docs: Added Docs for Wishlist Entity
    Added Docs for Wishlist Entity

--- app/src/main/java/com/example/wave/Entities/WishlistOrder.java
+++ app/src/main/java/com/example/wave/Entities/WishlistOrder.java
@@ -5,6 +5,16 @@ public class WishlistOrder extends Order{
     private String artistName;
     private String releaseImage;
 
+    /**
+     * Constructor for WishlistOrder
+     * @param orderID
+     * @param type
+     * @param userID
+     * @param discographyID
+     * @param releaseName
+     * @param artistName
+     * @param releaseImage
+     */
     public WishlistOrder(String orderID, String type, String userID, String discographyID, String releaseName, String artistName, String releaseImage) {
         super(orderID, type, userID, discographyID);
         this.releaseName = releaseName;
@@ -12,33 +22,64 @@ public class WishlistOrder extends Order{
         this.releaseImage = releaseImage;
     }
 
+    /**
+     * Empty constructor for WishlistOrder
+     */
     public WishlistOrder() {
     }
 
+    /**
+     * Getter for releaseName
+     * @return releaseName
+     */
     public String getReleaseName() {
         return releaseName;
     }
 
+    /**
+     * Setter for releaseName
+     * @param releaseName
+     */
     public void setReleaseName(String releaseName) {
         this.releaseName = releaseName;
     }
 
+    /**
+     * Getter for artistName
+     * @return artistName
+     */
     public String getArtistName() {
         return artistName;
     }
 
+    /**
+     * Setter for artistName
+     * @param artistName
+     */
     public void setArtistName(String artistName) {
         this.artistName = artistName;
     }
 
+    /**
+     * Getter for releaseImage
+     * @return releaseImage
+     */
     public String getReleaseImage() {
         return releaseImage;
     }
 
+    /**
+     * Setter for releaseImage
+     * @param releaseImage
+     */
     public void setReleaseImage(String releaseImage) {
         this.releaseImage = releaseImage;
     }
 
+    /**
+     * toString method for WishlistOrder
+     * @return String
+     */
     @Override
     public String toString() {
         return super.toString() +
